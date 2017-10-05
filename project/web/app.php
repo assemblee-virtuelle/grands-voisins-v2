d8616e2504c27f47fcfeec78ed95aa194f1f099b
Romain WEEGER <romain.weeger@gmail.com>
2017-10-05 17:21:48 +0200

    Dockerized in local

--- project/web/app.php
+++ project/web/app.php
@@ -2,6 +2,9 @@
 
 use Symfony\Component\HttpFoundation\Request;
 
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 /** @var \Composer\Autoload\ClassLoader $loader */
 $loader = require __DIR__.'/../app/autoload.php';
 include_once __DIR__.'/../var/bootstrap.php.cache';
